1dbc14840a6c9241c91f8531df9db70ee5220db3
Enmin Zhou <zhouem14@gmail.com>
2020-11-24 01:39:56 -0500

    fix api problem

--- src/stock_spider.py
+++ src/stock_spider.py
@@ -1,14 +1,34 @@
 import requests
 import pandas as pd
-from bs4 import BeautifulSoup
+from datetime import datetime
 from mongodb import *
 import json
 
 dbname = "stock"
 collection_name = "historical"
 
+url = 'https://query1.finance.yahoo.com/v8/finance/chart/{}'
 
-def get_stock_data():
+
+def get_stock_data(code):
+    period = "1y"
+    interval = "1d"
+    params = {}
+    params["range"] = period
+    params["interval"] = interval.lower()
+    res = requests.get(url.format(code), params=params)
+    res_json = json.loads(res.text)
+    data = res_json['chart']['result'][0]
+    timestamps = data['timestamp']
+    date = [datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d') for timestamp in timestamps]
+    indicators_data = data['indicators']['quote'][0]
+    volume = indicators_data['volume']
+    close = indicators_data['close']
+    open = indicators_data['open']
+    high = indicators_data['high']
+    low = indicators_data['low']
+    df = pd.DataFrame({'date':date, 'volume':volume, 'close':close, 'open':open, 'high': high, 'low':low})
+    return df
 
 
 def create_collection(handle):
@@ -24,8 +44,7 @@ def insert(data, handle):
 
 
 if __name__ == "__main__":
-    data = get_stock_data()
-    exit()
+    data = get_stock_data('AAPL')
     db = connect(dbname)
     col = create_collection(db)
     insert(data, col)
